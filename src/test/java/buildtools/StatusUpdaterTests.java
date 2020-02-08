1256007e9bc7d20c0090da6bf76c547a1b207b6b
Adam Björnberg <38786385+adbjo@users.noreply.github.com>
2020-02-08 20:42:01 +0100

    Unit-tests for StatusUpdater.java
    Adds unit-tests for StatusUpdater.java

--- src/test/java/buildtools/StatusUpdaterTests.java
+++ src/test/java/buildtools/StatusUpdaterTests.java
@@ -1,33 +1,75 @@
 package buildtools;
 
 import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class StatusUpdaterTests {
+    /**
+     * Tests if the GitHub API is reachable.
+     */
+    @Test
+    public void test0() {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet("https://api.github.com/orgs/octokit/repos");
+        try {
+            CloseableHttpResponse response = client.execute(request);
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    // TODO
-    // Test if server can reach the GitHub api.
-
-    // TODO
-    // Tests if http POST is properly constructed. WIP.
+    /**
+     * Tests if the createHttpPost function creates the HTTP POST request correctly.
+     */
     @Test
     public void test1() {
         String owner = "adbjo";
         String repo = "DD2480-group-18-CI";
         String commitSha = "afda99d6c1462f549e27dd86c0a6918194c4a7f6";
+        Build.Result status = Build.Result.success;
         String token = "9d876fs987f9s87fd6s987fsd98f7s698f7ds69f";
 
-        HttpPost httpPost = StatusUpdater.createHttpPost(owner, repo, commitSha, Build.Result.success, token);
+        HttpPost httpPost = StatusUpdater.createHttpPost(owner, repo, commitSha, status, token);
 
-        Header[] headers = httpPost.getAllHeaders();
-        for (Header h: headers) {
+        assertEquals("POST", httpPost.getMethod());
+        assertEquals("api.github.com", httpPost.getURI().getAuthority());
+        assertEquals(
+                "/repos/adbjo/DD2480-group-18-CI/statuses/afda99d6c1462f549e27dd86c0a6918194c4a7f6",
+                httpPost.getURI().getPath()
+        );
+        boolean hasAuthHeader = false;
+        for (Header h : httpPost.getAllHeaders()) {
             if (h.getName().equals("Authorization")) {
-
+                hasAuthHeader = true;
+                assertEquals("token 9d876fs987f9s87fd6s987fsd98f7s698f7ds69f", h.getValue());
             }
         }
-
-        assertTrue(false);
+        assertTrue(hasAuthHeader);
+        try {
+            assertEquals(
+                    new JSONObject(EntityUtils.toString(httpPost.getEntity(), "UTF-8")).toString(),
+                    new JSONObject("{" +
+                            "state:'success'," +
+                            "target_url:'https://www.google.se'," +
+                            "description:'Success'," +
+                            "context:'mobergliuslefors'}").toString()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
