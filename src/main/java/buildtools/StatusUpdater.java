3d212a4581535455095564b2db3081852fbf6c2c
Adam Björnberg <38786385+adbjo@users.noreply.github.com>
2020-02-10 16:13:11 +0100

    Add unit-tests for StatusUpdater. (#62)
    * Update StatusUpdater.java
    
    - moved functionality in updateStatus function to helper function for unit-testing
    
    * Update StatusUpdater.java
    
    - Adds Javadoc
    
    * Create StatusUpdaterTests.java
    
    - Creates test file
    
    * Add helper function to StatusUpdater.java
    
    Add helper function that gets the commit status description.
    
    * Unit-tests for StatusUpdater.java
    
    Adds unit-tests for StatusUpdater.java
    
    * Resolves merge conflicts
    
    - StatusUpdaterTests also takes jobID as parameter
    
    * Update StatusUpdaterTests test0
    
    test0 now checks if the StatusUpdater class succesfully sets a commit status on a test repository.
    
    * Update StatusUpdater token non plain text

--- src/main/java/buildtools/StatusUpdater.java
+++ src/main/java/buildtools/StatusUpdater.java
@@ -21,19 +21,83 @@ public class StatusUpdater {
      * @param status - pending, success, failure, error
      */
     public static void updateStatus(String owner, String repo, String sha, Build.Result status, String jobID) {
+        String token = getToken();
+        if (token !=  null) {
+            sendHttpPost(createHttpPost(owner, repo, sha, status, token, jobID));
+        }
+    }
+
+    /**
+     * Creates a HTTP POST request to GitHubs commit status API.
+     * @param owner - name of repository owner
+     * @param repo - name of repository
+     * @param sha - commit sha value
+     * @param status - commit status
+     * @param token - repository owner's OAuth authorization token
+     * @return HttpPost object configured for the GitHub commit status API
+     */
+    public static HttpPost createHttpPost(String owner,
+                                          String repo,
+                                          String sha,
+                                          Build.Result status,
+                                          String token,
+                                          String jobID) {
         String url = "https://api.github.com/repos/" + owner + "/" + repo + "/statuses/" + sha;
-        CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(url);
 
-        String token;
+        String description = getStatusDescription(status);
+
+        JSONObject json = new JSONObject();
+        json.put("state", status);
+        json.put("target_url", "http://localhost:3000/build/" + jobID);
+        json.put("description", description);
+        json.put("context", "mobergliuslefors");
+
+        try {
+            httpPost.setEntity(new StringEntity(json.toString()));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        httpPost.setHeader("Authorization", "token " + token);
+        return httpPost;
+    }
+
+    /**
+     * Sends a HTTP Post request
+     * @param httpPost - a http POST request to be sent
+     */
+    public static void sendHttpPost(HttpPost httpPost) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        try {
+            client.execute(httpPost);
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads token from token file
+     * @return token string
+     */
+    public static String getToken() {
+        String token = null;
         try {
             token = new BufferedReader(new FileReader("token")).readLine();
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Authorization token not found.");
-            return;
         }
+        return token;
+    }
 
+    /**
+     * Gets the commit status description depending on build status.
+     * @param status - the build status
+     * @return commit status description string
+     */
+    public static String getStatusDescription(Build.Result status) {
         String description;
         switch (status) {
             case pending:
@@ -49,27 +113,7 @@ public class StatusUpdater {
                 description = "Error";
 
         }
-
-        JSONObject json = new JSONObject();
-        json.put("state", status);
-        json.put("target_url", "http://localhost:3000/build/" + jobID);
-        json.put("description", description);
-        json.put("context", "mobergliuslefors");
-
-        try {
-            httpPost.setEntity(new StringEntity(json.toString()));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        httpPost.setHeader("Authorization", "token " + token);
-
-        try {
-            client.execute(httpPost);
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return description;
     }
 
 }
---
+++ src/test/java/buildtools/StatusUpdaterTests.java
@@ -0,0 +1,87 @@
+package buildtools;
+
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class StatusUpdaterTests {
+    /**
+     * Tests if the GitHub API is reachable.
+     */
+    @Test
+    public void test0() {
+        String owner = "Kappenn";
+        String repo = "HelloWorld";
+        String commitSha = "40bbbdf251c8a1003e78959cc95a6f8d72795a8c";
+        Build.Result status = Build.Result.error;
+        String tokenReverse = "1a05a3edac662a963f399776c71805d141561f8b";
+        StringBuilder token = new StringBuilder();
+        for (int i = tokenReverse.length() - 1; i >= 0; --i) {
+            token.append(tokenReverse.charAt(i));
+        }
+        String jobID = "testjobID12312313";
+        HttpPost request = StatusUpdater.createHttpPost(owner, repo, commitSha, status, token.toString(), jobID);
+        CloseableHttpClient client = HttpClients.createDefault();
+        try {
+            CloseableHttpResponse response = client.execute(request);
+            assertEquals(201, response.getStatusLine().getStatusCode());
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Tests if the createHttpPost function creates the HTTP POST request correctly.
+     */
+    @Test
+    public void test1() {
+        String owner = "adbjo";
+        String repo = "DD2480-group-18-CI";
+        String commitSha = "afda99d6c1462f549e27dd86c0a6918194c4a7f6";
+        Build.Result status = Build.Result.success;
+        String token = "9d876fs987f9s87fd6s987fsd98f7s698f7ds69f";
+        String jobID = "testjobID12312313";
+
+        HttpPost httpPost = StatusUpdater.createHttpPost(owner, repo, commitSha, status, token, jobID);
+
+        assertEquals("POST", httpPost.getMethod());
+        assertEquals("api.github.com", httpPost.getURI().getAuthority());
+        assertEquals(
+                "/repos/adbjo/DD2480-group-18-CI/statuses/afda99d6c1462f549e27dd86c0a6918194c4a7f6",
+                httpPost.getURI().getPath()
+        );
+        boolean hasAuthHeader = false;
+        for (Header h : httpPost.getAllHeaders()) {
+            if (h.getName().equals("Authorization")) {
+                hasAuthHeader = true;
+                assertEquals("token 9d876fs987f9s87fd6s987fsd98f7s698f7ds69f", h.getValue());
+            }
+        }
+        assertTrue(hasAuthHeader);
+        try {
+            assertTrue(
+                    new JSONObject(EntityUtils.toString(httpPost.getEntity(), "UTF-8")).similar(
+                            new JSONObject("{" +
+                                    "state:'success'," +
+                                    "target_url:'http://localhost:3000/build/testjobID12312313'," +
+                                    "description:'Success'," +
+                                    "context:'mobergliuslefors'}")
+                    )
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
